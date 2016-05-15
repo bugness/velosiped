dd0d179417275a2080ea748ea3653e2c367b1a1f
bugness <bugness@yandex.ru>
2016-05-15 15:14:29 +0700

    added tests for Router

---
+++ tests/RouterTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Velosiped;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+    protected $map = [];
+    protected $router;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->map = [
+            'index' => 'IndexController',
+            'users' => 'UsersController',
+            'test'  => Router::class,
+        ];
+        $this->router = new Router;
+        $this->router->setRoutes($this->map);
+    }
+
+    public function testSuccessCreate()
+    {
+        $this->assertInstanceOf(Router::class, new Router);
+    }
+
+    public function testSetRoutes()
+    {
+        $this->assertInstanceOf(
+            Router::class,
+            $this->router->setRoutes($this->map)
+        );
+    }
+
+    public function testGetRoutes()
+    {
+        $this->assertEquals($this->map, $this->router->getRoutes());
+    }
+
+    public function testSuccessGetController()
+    {
+        $this->assertInstanceOf(
+            Router::class,
+            $this->router->getController('test')
+        );
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testControllerNotFound()
+    {
+        $this->router->getController('error');
+    }
+}
